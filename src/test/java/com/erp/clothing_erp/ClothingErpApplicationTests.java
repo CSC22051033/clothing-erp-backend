e3e578fc9d7274925f064bfbf7dfc9757bea315f
CSC22051033 <841493197@qq.com>
2026-05-27 17:00:06 +0800

    初始化后端项目：Spring Boot 3.x + JPA + MySQL 基础配置

---
+++ src/test/java/com/erp/clothing_erp/ClothingErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.clothing_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
